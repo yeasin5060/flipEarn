cb1753a381b070e6ee38a72d754c0738a85d5e9d
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-01-19 18:25:10 +0600

    complete the routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
-function App() {
-  
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import MarketPlace from "./pages/MarketPlace"
+import MyListings from "./pages/MyListings"
+import ListingDetails from "./pages/ListingDetails"
+import ManageListing from "./pages/ManageListing"
+import Messages from "./pages/Messages"
+import MyOrders from "./pages/MyOrders"
+import Loading from "./pages/Loading"
 
+function App() {
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">hello</h1>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element ={<Home/>}/>
+        <Route path="/makerPlace" element ={<MarketPlace/>}/>
+        <Route path="/my-listing" element ={<MyListings/>}/>
+        <Route path="/listing/:listingId" element ={<ListingDetails/>}/>
+        <Route path="/create-listing" element ={<ManageListing/>}/>
+        <Route path="/edit-listing/id" element ={<ManageListing/>}/>
+        <Route path="/message" element ={<Messages/>}/>
+        <Route path="/my-order" element ={<MyOrders/>}/>
+        <Route path="/loading" element ={<Loading/>}/>
+      </Routes>
+    </div>
   )
 }
 
