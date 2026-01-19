fd8653c558f1a21415f687aa1bb9ef492236ffb1
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-01-19 18:52:05 +0600

    complete the hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <div>
-        <h1>
-            home page
-        </h1>
+       <Hero/>
     </div>
   )
 }
