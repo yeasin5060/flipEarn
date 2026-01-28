4af7cb2ab33169c490fb0d0c8ea92753cc7e36e9
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-01-28 20:32:41 +0600

    add the onClearFilter function

--- client/src/components/FilterSidebar.jsx
+++ client/src/components/FilterSidebar.jsx
@@ -62,7 +62,22 @@ const FilterSidebar = ({showFilterphone,setShowFilterPhone,filters , setFilters}
     {value : 'sports' , label : 'Sports'},
     {value : 'health' , label : 'Health'},
     {value : 'finance' , label : 'Finance'},
-  ]
+  ];
+
+  const onClearFilters = ()=> {
+    if(search){
+      navigate('/marketplace')
+    };
+    setFilters({
+      platform : null,
+      maxPrice : 100000,
+      minFollowers : 0,
+      niche : null,
+      verified : false,
+      monetized : false
+    });
+  };
+
   return (
     <div className={`${showFilterphone ? 'max-sm:fixed ': 'max-sm:hidden'}max-sm:inset-0 z-100 max-sm:h-screen max-sm:overflow-scroll bg-white rounded-lg shadow-sm border border-gray-200 h-fit sticky top-24 md:min-w-[300px]`}>
       <div className='p-4 border-b border-gray-200'>
@@ -72,7 +87,7 @@ const FilterSidebar = ({showFilterphone,setShowFilterPhone,filters , setFilters}
             <h3 className='font-semibold capitalize'>filters</h3>
           </div>
           <div className='flex items-center gap-2'>
-            <X className='size-6 text-gray-500 hover:text-gray-700 p-1 hover:bg-gray-100 rounded transition-colors cursor-pointer'/>
+            <X onClick={onClearFilters} className='size-6 text-gray-500 hover:text-gray-700 p-1 hover:bg-gray-100 rounded transition-colors cursor-pointer'/>
             <button onClick={()=> setShowFilterPhone(false)} className='sm:hidden text-sm border text-gray-700 px-3 py-1 rounded'>apply</button>
           </div>
         </div>
