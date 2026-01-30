79ef28e17d9a6129d25eba635426823ff7ef366a
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-01-30 13:58:08 +0600

    add the previous button in listingdetails page

--- client/src/pages/ListingDetails.jsx
+++ client/src/pages/ListingDetails.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { getProfileLink } from '../assets/assets';
+import { useSelector } from 'react-redux';
+import { ArrowLeftIcon, Loader2Icon } from 'lucide-react';
 
 const ListingDetails = () => {
-  return (
-    <div>ListingDetails page</div>
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY || '$';
+  const [listing , setListing] = useState(null);
+  const profileLink = listing && getProfileLink(listing.platform , listing.username);
+
+  const {listingId} = useParams();
+  const {listings} = useSelector(state => state.listing);
+
+  useEffect(()=> {
+    const listing = listings.find((listing)=> listing.id === listingId);
+    if(listing){
+      setListing(listing)
+    }
+  },[listings,listingId])
+  return listing ? (
+    <div className='mx-auto min-h-screen px-6 md:px-16 lg:px-24 xl:px-32'>
+      <button onClick={()=> navigate(-1)} className='flex items-center gap-2 text-slate-600 py-5'>
+        <ArrowLeftIcon className='size-4'/> go to previous page
+      </button>
+
+      <div className='flex items-start max-md:flex-col gap-10'>
+        <div className='flex-1 max-md:w-full'>
+
+        </div>
+        {/* Seller Info $ Purchase Option  */}
+        <div></div>
+      </div>
+    </div>
+  ): (
+    <div className='h-screen flex items-center justify-center'>
+      <Loader2Icon className='size-7 animate-spin text-indigo-600'/>
+    </div>
   )
 }
 
--- client/src/pages/ManageListing.jsx
+++ client/src/pages/ManageListing.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ManageListing = () => {
   return (
     <div>
-      ManageListing page
+      
     </div>
   )
 }
