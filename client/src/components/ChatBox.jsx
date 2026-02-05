b40d2a69358e16d48ec2f96599d989f90f9f96bf
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-02-05 21:28:31 +0600

    add the message date time

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { dummyChats } from '../assets/assets';
-import { X } from 'lucide-react';
+import { Loader2Icon, X } from 'lucide-react';
 import { clearChat } from '../app/features/chatSlice.js';
+import {format} from 'date-fns'
 
 const ChatBox = () => {
      const dispatch = useDispatch()
@@ -49,6 +50,33 @@ const ChatBox = () => {
                 <button onClick={()=> dispatch(clearChat())} className='ml-4 p-1 hover:bg-white/20 hover:bg-opacity-20 rounded-lg transition-colors'>
                     <X className='w-5 h-5'/>
                 </button>
+            </div>
+
+            {/*Message Area */}
+            <div className='flex-1 overflow-y-auto p-4 space-y-4 bg-gray-100'>
+                {
+                    isLoding ?(
+                        <div className='flex items-center justify-center h-full'>
+                            <Loader2Icon className='size-6 animate-spin text-indigo-600'/>
+                        </div>
+                    ): messages.length === 0 ? (
+                        <div className='flex items-center justify-center h-full'>
+                            <div className='text-center'>
+                                <p className='text-gray-500 mb-2'>No message yet</p>
+                                <p className='text-sm text-gray-400'>Start the conversation</p>
+                            </div>
+                        </div>
+                    ) : (
+                        messages.map((message)=> (
+                            <div key={message.id} className={`flex ${message.sender_id === user.id ? 'justify-end' : 'justify-start'}`}>
+                                <div className={`max-w-[70%] rounded-lg p-3 pb-1 ${message.sender_id === user.id ? 'bg-indigo-600 text-white' : 'bg-white border border-gray-200 text-gray-800'}`}>
+                                    <p className='text-sm whitespace-pre-wrap break-words '>{message.message}</p>
+                                    <p className={`text-[10px] mt-1 ${message.sender_id === user.id ? 'text-indigo-200': 'text-gray-400'}`}>{format(new Date(message.createdAt), 'MMM dd "at" h:mm a')}</p>
+                                </div>
+                            </div>
+                        ))
+                    )
+                }
             </div>
         </div>
     </div>
