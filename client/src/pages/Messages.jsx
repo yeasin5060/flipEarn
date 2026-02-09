cce7ba128e5f1da8a8b0a6cf68b3433e261e8ccd
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-02-09 16:47:01 +0600

    add the header section in message page

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyChats } from '../assets/assets';
 
 const Messages = () => {
+  const user = {id : 'user_1'};
+
+  const [chats , setChats] = useState([]);
+  const [searchQuery , setSeachQuery] = useState('');
+  const [loading , setLoading] = useState(true);
+
+  const fetchUserChats = async ()=> {
+    setChats(dummyChats);
+    setLoading(false);
+  };
+
+  useEffect(()=> {
+    fetchUserChats();
+    const interval = setInterval(()=> {
+      fetchUserChats();
+    },10*1000);
+    return ()=> clearInterval(interval);
+  },[]);
+
   return (
-    <div>
-      Messages page
+    <div className='mx-auto min-h-screen px-6 md:px-16 lg:px-24 xl:px-32'>
+      <div className='py-10'>
+        {/*Header */}
+        <div className='mb-8'>
+          <h1 className='text-3xl font-bold text-gray-800 mb-2 capitalize'>message</h1>
+          <p className='text-gray-600'>chat with Buyer and sellers</p>
+        </div>
+      </div>
     </div>
   )
 }
