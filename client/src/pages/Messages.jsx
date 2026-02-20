390ffda1926e62fb2fbbf275aa2fbda96af37f4c
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-02-20 19:16:32 +0600

    complete the message page

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useMemo, useState } from 'react'
 import { dummyChats } from '../assets/assets';
 import { MessageCircle, Search } from 'lucide-react';
 import {format, isToday, isYesterday, parseISO } from 'date-fns'
+import {useDispatch} from 'react-redux'
+import { setChat } from '../app/features/chatSlice.js';
 
 const Messages = () => {
+  const dispatch = useDispatch();
   const user = {id : 'user_1'};
 
   const [chats , setChats] = useState([]);
@@ -33,6 +36,10 @@ const Messages = () => {
       return chat.listing?.title?.toLowerCase().includes(query) || chatuser?.name?.toLowerCase().includes(query)
     })
   },[chats,searchQuery])
+
+  const handleOpenChat = (chat) => {
+    dispatch(setChat({listing : chat.listing , chatId : chat.id}))
+  }
 
   const fetchUserChats = async ()=> {
     setChats(dummyChats);
@@ -81,7 +88,7 @@ const Messages = () => {
                 filteredChets.map((chat)=> {
                   const chatUser = chat.chatUserId === user?.id ? chat.ownerUser : chat.chatUser;
                   return (
-                    <button key={chat.id} className='w-full p-4 hover:bg-gray-50 transition-colors text-left'>
+                    <button onClick={()=> handleOpenChat(chat)} key={chat.id} className='w-full p-4 hover:bg-gray-50 transition-colors text-left'>
                       <div className='flex items-start space-x-4'>
                         <div className='flex-shrink-0'>
                           <img className='w-12 h-12 rounded-lg object-cover' src={chatUser?.image} alt={chat?.chatUser?.name} />
