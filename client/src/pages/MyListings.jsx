412b9a3c878804963eb9bbefcb66526508f9965d
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-02-21 19:00:11 +0600

    start the mylisting page

--- client/src/pages/MyListings.jsx
+++ client/src/pages/MyListings.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import {useNavigate} from 'react-router-dom'
 
 const MyListings = () => {
+  const {userListing ,  balance } = useSelector((state)=> state.listing);
+  const currency = import.meta.env.VITE_CURRENCY  || '$';
+  const navigate = useNavigate();
+  const totalValue = userListing.reduce((sum , listing)=> sum + (listing.price || 0), 0)
   return (
     <div>
       MyListings page
