f03d6d0f40f5fe48dd315b5cc27383f55f061ad9
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-02-25 11:38:22 +0600

    complete the balance section in mylisting page

--- client/src/pages/MyListings.jsx
+++ client/src/pages/MyListings.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import {useNavigate} from 'react-router-dom'
-import { CheckCircle, DollarSign, Eye, Plus, TrendingUp } from 'lucide-react';
+import { ArrowDownCircleIcon, CheckCircle, CoinsIcon, DollarSign, Eye, Plus, TrendingUp, WalletIcon } from 'lucide-react';
 import StatCard from '../components/StatCard';
 
 const MyListings = () => {
@@ -34,6 +34,27 @@ const MyListings = () => {
         <StatCard title='sold' value={soldListings} icon={<TrendingUp className='size-6 text-indigo-600' />} color='indigo'/>
 
         <StatCard title='Total Value' value={`${currency} ${totalValue.toLocaleString()}`} icon={<DollarSign className='size-6 text-yellow-600'/>}  color='yellow'/>
+      </div>
+      {/*Balance Section */}
+      <div className='flex flex-col sm:flex-row justify-between gap-4 xl:gap-20 p-6 mb-10 bg-white rounded-xl broder border-gray-200'>
+        {
+          [
+            {lable: 'Earned', value: balance.earned, icon: WalletIcon},
+            {lable: 'Withdrawn', value: balance.withdrawn, icon: ArrowDownCircleIcon},
+            {lable: 'Available', value: balance.available, icon: CoinsIcon}
+          ].map((item, index)=>(
+            <div key={index} className='flex flex-1 items-center justify-between p-4 rounded-lg border border-gray-100 cursor-pointer'>
+              <div className='flex items-center gap-3'>
+                <item.icon className='text-gray-500 w-6 h-6'/>
+                <span className='text-gray-600 font-medium'>{item.lable}</span>
+              </div>
+              <span className='text-xl font-medium text-gray-700'>
+                {currency}
+                {item.value.toFixed(2)}
+              </span>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
