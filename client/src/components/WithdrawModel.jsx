c51bd6448a6eb363145a54ff8644271d99fe8ded
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-03-14 12:50:19 +0600

    header section compelete the withdrawmodel component

---
+++ client/src/components/WithdrawModel.jsx
@@ -0,0 +1,36 @@
+import { X } from 'lucide-react';
+import React, { useState } from 'react'
+
+const WithdrawModel = ({onClose}) => {
+  const [amount , setAmount] = useState('');
+  const [account , setAccount] = useState([
+    {type : 'text', name : 'Account Holder Name', value : ''},
+    {type : 'text', name : 'Bank Name', value : ''},
+    {type : 'number', name : 'Account Number', value : ''},
+    {type : 'text', name : 'Account Type', value : ''},
+    {type : 'text', name : 'SWIFT', value : ''},
+    {type : 'text', name : 'Brance', value : ''}
+  ]);
+
+  const handleSubmission = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-100 flex items-center justify-center sm:p-4'>
+      <div className='bg-white sm:rounded-lg shadow-2xl w-full max-w-lg h-screen sm:h-[320px] flex flex-col'>
+        {/*Header*/}
+        <div className='bg-gradient-to-r from-indigo-600 to-indigo-400 text-white p-4 sm:rounded-t-lg flex items-center justify-between'>
+          <div className='flex-1 min-w-0'>
+            <h3 className='text-lg font-semibold truncate capitalize'>withdraw found</h3>
+          </div>
+          <button onClick={onClose} className='ml-4 p-1 hover:bg-white/20 hover:bg-opacity-20 rounded-lg transition-colors'>
+            <X className='w-5 h-5'/>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WithdrawModel
