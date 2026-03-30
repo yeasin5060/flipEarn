202f520b085b350c6b7a75be8d70fc3a7cb1ecd1
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-03-30 21:14:30 +0600

    start the managelisting page

--- client/src/pages/ManageListing.jsx
+++ client/src/pages/ManageListing.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom'
 
 const ManageListing = () => {
+  const {id} = useParams();
+  const navigate = useNavigate();
+  const {userListing} = useSelector((state)=> state.listing);
+
+  const[logingListing,setLodingLsting] = useState(false);
+  const[isEditing,srtIsEditing] = useState(false);
+
+  const [formData , setFormData] = useState({
+    title : '',
+    platform : '',
+    username : '',
+    followers_count : '',
+    engament_rent : '',
+    monthly_views : '',
+    niche : '',
+    price : '',
+    description : '',
+    verified : false ,
+    monetized : false,
+    country : '',
+    age_range : '',
+    image : []
+  });
+
+  const platForms = ['youtube','instagram','tiktok','facebook','twitter','linkedin','pinterest','snapchat','twitch','discord']
+
+  const niches = ['lifestyle','fitness','food','travel','tech','gaming','fashion','beauty','business','education','entertainment','music','art','sports','health','finance','other']
   return (
     <div>
       
