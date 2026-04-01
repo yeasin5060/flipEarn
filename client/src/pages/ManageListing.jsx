1e84b107d3bb4bb9a596c17bba4e2b404746b086
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-01 11:09:06 +0600

    add the handleImageUpload function in manageListing page

--- client/src/pages/ManageListing.jsx
+++ client/src/pages/ManageListing.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom'
+import toast from 'react-hot-toast';
 
 const ManageListing = () => {
   const {id} = useParams();
@@ -24,12 +25,27 @@ const ManageListing = () => {
     monetized : false,
     country : '',
     age_range : '',
-    image : []
+    images : []
   });
 
-  const platForms = ['youtube','instagram','tiktok','facebook','twitter','linkedin','pinterest','snapchat','twitch','discord']
+  const platForms = ['youtube','instagram','tiktok','facebook','twitter','linkedin','pinterest','snapchat','twitch','discord'];
 
-  const niches = ['lifestyle','fitness','food','travel','tech','gaming','fashion','beauty','business','education','entertainment','music','art','sports','health','finance','other']
+  const niches = ['lifestyle','fitness','food','travel','tech','gaming','fashion','beauty','business','education','entertainment','music','art','sports','health','finance','other'];
+
+  const ageRanges = ['13-17 years','18-24 years','25-34 years','35-44 years','45-54 years','55+ years','Mixed ages'];
+
+  const handleInputChange = (field , value) => {
+    setFormData((prev)=> ({...prev,[field] : value}));
+  };
+
+  const handleImageUpload = async (event)=> {
+    const files = Array.from(event.target.files);
+    if(!files.length) return ;
+    if(files.length + formData.images.length > 5) return toast.error('You can add up to 5 image');
+    setFormData((prev)=> ({...prev,images : [...prev.images, ...files]}))
+  };
+
+  
   return (
     <div>
       
