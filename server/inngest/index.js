d1a3686e4ad6e2d249b45c6eadf8c53a0ddb3f43
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-26 15:58:27 +0600

    server error solve

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import { prisma } from "../src/db";
+import { prisma } from "../src/db.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "profile-marketplace" });
--- server/src/db.js
+++ server/src/db.js
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from '@prisma/adapter-neon'
 const adapter = new PrismaNeon({
   connectionString: process.env.DATABASE_URL,
