aef444cec701d6fd5581566f4c220f516734885a
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-04-30 11:18:20 +0600

    cors update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(clerkMiddleware());
 
 const PORT = process.env.PORT || 7000;
