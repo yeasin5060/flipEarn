abd03e4017246605ec8e374640c98639cbcc55e7
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2026-05-05 07:55:57 +0600

    complete the protectAdmin middleware

--- server/middlewares/authmiddleware.js
+++ server/middlewares/authmiddleware.js
@@ -1,3 +1,5 @@
+import {clerkClient} from '@clerk/express'
+
 
 export const protect = async (req , res , next) => {
     try {
@@ -8,6 +10,22 @@ export const protect = async (req , res , next) => {
         }
         const hasPremiumPlan = await has({plan : 'premium'});
         req.plan = hasPremiumPlan ? 'premium' : 'free';
+        return next()
+    } catch (error) {
+        console.log(error);
+        
+        return res.status(401).json({message : error.message});
+    }
+}
+
+export const protectAdmin = async (req , res , next) => {
+    try {
+        const {user} = await clerkClient.users.getUser(await req.auth().userId);
+        const isAdmin = process.env.ADMIN_EMAILS.split(',').includes(user.emailAddresses[0].emailAddress);
+
+        if(!isAdmin){
+            return res.status(401).json({message :'Unauthorized'});
+        }
         return next()
     } catch (error) {
         console.log(error);
